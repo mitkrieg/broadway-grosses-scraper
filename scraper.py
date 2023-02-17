7e30c65c59196795fd2511649d50a540dbb97f78
Mitchell Krieger <mitkrieger@gmail.com>
2023-02-16 22:28:14 -0500

    switch to headless and fix cleaning

--- scraper.py
+++ scraper.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
-import codecs
 import re
 import time
 import pandas as pd
@@ -15,7 +14,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 timer = time.time()
 
-driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+options = Options()
+options.add_argument('--headless')
+
+driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 url = 'https://www.broadwayleague.com/research/grosses-broadway-nyc/'
 wait = WebDriverWait(driver,10)
 driver.get(url)
@@ -25,7 +27,7 @@ end_date = date.today()
 all_dates = [start_date + timedelta(days=7*(x+1)) for x in range((end_date - start_date).days // 7)]
 dates = [d.strftime('%m/%d/%Y') for d in all_dates if d <= date(2020,3,8) or d >= date(2021,8,8)]
 
-all_cols = ['week_of','show_name','show_type','theatre','num_performances_prev','num_performances',
+all_cols = ['week_of','show_name','show_type','theatre','num_previews','num_performances',
                'grosses','grosses_prev','attendance','attend_prev','pct_capacity'
            ]
 
@@ -34,6 +36,7 @@ cols = [col for col in all_cols if col.endswith('_prev') == False]
 
 shows = pd.DataFrame(columns=cols)
 weekly = []
+
 for week in dates:
     print(f'getting {week}')
     get_url = driver.current_url
@@ -69,16 +72,21 @@ for week in dates:
     shows = pd.concat([shows,table[cols]])
     time.sleep(random.randint(0, 3))
 
-shows.applymap(lambda x:x.getText())
+shows = shows.applymap(lambda x:x.getText())
 shows.week_of = pd.to_datetime(shows.week_of)
-shows.grosses = shows.grosses.str.replace('[ $,]','',regex=True).astype(int)
+shows.grosses = shows.grosses.str.replace('[ $,]','',regex=True).astype(float)
 shows.attendance = shows.attendance.str.replace('[ $,]','',regex=True).astype(int)
+shows.num_performances = shows.num_performances.astype(int)
+shows.num_previews = shows.num_previews.astype(int)
 shows.pct_capacity = shows.pct_capacity.str.replace('[ %]','',regex=True).astype(float) / 100
-shows.to_csv('./data/shows.csv',header=True)
+shows.to_csv('./data/shows.csv',header=True, index=False)
 
 weeks = pd.DataFrame(weekly)
+weeks.totalentries = weeks.totalentries.astype(int)
+weeks.totalgross = weeks.totalgross.astype(float)
+weeks.totalattendance = weeks.totalattendance.astype(int)
 weeks.week_of = pd.to_datetime(weeks.week_of)
-weeks.to_csv('./data/weeks.csv',header=True)
+weeks.to_csv('./data/weeks.csv',header=True, index=False)
 
 print(f'Done in {time.time() - timer} seconds')
 
